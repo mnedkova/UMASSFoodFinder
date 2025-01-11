acb74e70366f55b70c166a79b1b88d61d42ad0d6
Maya Nedkova <mnedkova@umass.edu>
2025-01-11 12:46:12 -0500

    WIP

--- lambdas/scraper/index.py
+++ lambdas/scraper/index.py
@@ -13,7 +13,14 @@ import requests
 
 #Base_URL: https://umassdining.com/foodpro-menu-ajax?tid={id}&date="{date}"
 
+
+
 def lambda_handler(event, context):
+    
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket("umassdininginfo")
+    
+    
     tid = {
         1: "Worcester",
         2: "Franklin",
@@ -21,22 +28,43 @@ def lambda_handler(event, context):
         4: "Berkshire",  
     }
     
-    
     # For loop iterates through each dining hall for each specified day to form desired link
     for location_id, location_name in tid.items():
-        base_url = f'https://umassdining.com/foodpro-menu-ajax?tid={location_id}&date='
-        
-        
         MAX_DAYS = 14
-        current_day = datetime.date.today()
-        print(current_day)
+        today = datetime.date.today()
         # iterates through each day
         for options in range(MAX_DAYS):
-            current_day = current_day + datetime.timedelta(days=1)
+            current_day = today + datetime.timedelta(days=options)
             print(current_day)
+            formatted_day = current_day.strftime('%m/%d/%Y')
+            url = f'https://umassdining.com/foodpro-menu-ajax?tid={location_id}&date={formatted_day}'
+            link = requests.get(url)
+            
+            if link.status_code != 200:
+                print(f'Error... {url} returned {link.status_code}')
+                continue
+            
+            try: 
+                json_response = link.json()
+                text_response = link.text()
+                text_object = text_response.encode("utf-8")
+                object_name = f'archive/{current_day}/{location_id}.json'
+                bucket.put_object(Key=object_name, Body=text_object)
+            except ValueError:
+                print(f'Error... no json response')
+                continue
+            
+
             
             
-        
+            
+            
+            
+            
+if __name__ == "__main__":
+    event = []
+    context = []
+    lambda_handler(event, context) 
         
     
     
