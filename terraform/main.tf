85f7ce2de759261fbebac6ad25210914df95a089
Maya Nedkova <mnedkova@umass.edu>
2025-01-16 16:09:27 -0500

    UFS-6 updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,6 +63,30 @@ resource "aws_iam_policy" "iam_policy_for_S3" {
 EOF
 }
 
+data "aws_iam_policy_document" "secrets_manager_policy_doc" {
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = [
+      "arn:aws:secretsmanager:us-east-2:717279727567:secret:prod/foodfinder*"
+    ]
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "secrets_manager_policy" {
+  name = "aws_iam_policy_for_lambda_to_access_secrets_manager"
+  path = "/"
+  policy = data.aws_iam_policy_document.secrets_manager_policy_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_secrets_manager_policy_to_iam_role" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.secrets_manager_policy.arn
+}
+
+
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
  role        = aws_iam_role.lambda_role.name
  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
@@ -73,23 +97,25 @@ resource "aws_iam_role_policy_attachment" "attach_iam_S3_policy_to_iam_role" {
   policy_arn = aws_iam_policy.iam_policy_for_S3.arn
 }
 
-resource "null_resource" "deployment_files" {
+resource "terraform_data" "build_lambda_scraper" {
   provisioner "local-exec" {
-    
+    command = "../scripts/create_pkg.sh"
+    working_dir = path.module
   }
 }
 
 data "archive_file" "zip_scraper" {
-type        = "zip"
-source_dir  = "${path.module}/../lambdas/scraper"
-output_path = "${path.module}/scraper.zip"
+  depends_on = [ terraform_data.build_lambda_scraper ]
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_dist_pkg"
+  output_path = "${path.module}/scraper.zip"
 }
 
 resource "aws_lambda_function" "scraper_lambda" {
-filename                       = "${path.module}/scraper.zip"
-function_name                  = "scraper_lambda_function"
-role                           = aws_iam_role.lambda_role.arn
-handler                        = "index.lambda_handler"
-runtime                        = "python3.8"
-depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename                       = "${path.module}/scraper.zip"
+  function_name                  = "scraper_lambda_function"
+  role                           = aws_iam_role.lambda_role.arn
+  handler                        = "index.lambda_handler"
+  runtime                        = "python3.13"
+  depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
