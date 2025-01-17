f15fe92739160f0e2bb40aed3d09ec8cb6e0d7ba
mnedkova <154064784+mnedkova@users.noreply.github.com>
2025-01-17 11:24:00 -0500

    6 ufs change lambda deployment and set to run automatically everyday (#7)
    * UFS-6 updated terraform
    
    * UFS-6 Modified Terraform to schedule daily executions and fixed errors
    
    ---------
    
    Co-authored-by: Maya Nedkova <mnedkova@umass.edu>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,67 +1,83 @@
-resource "aws_iam_role" "lambda_role" {
-name   = "Lambda_Function_Role"
-assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "lambda.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
+data "aws_iam_policy_document" "lambda_role_policy_doc" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    effect = "Allow"
+  }
 }
-EOF
+
+resource "aws_iam_role" "lambda_role" {
+  name   = "Lambda_Function_Role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_role_policy_doc.json
+}
+
+data "aws_iam_policy_document" "iam_policy_for_lambda_doc" {
+  statement {
+    actions = [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents",
+    ]
+    effect = "Allow"
+    resources = ["arn:aws:logs:*:*:*"]
+
+  }
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
  path         = "/"
  description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": [
-       "logs:CreateLogGroup",
-       "logs:CreateLogStream",
-       "logs:PutLogEvents"
-     ],
-     "Resource": "arn:aws:logs:*:*:*",
-     "Effect": "Allow"
-   }
- ]
+ policy = data.aws_iam_policy_document.iam_policy_for_lambda_doc.json
 }
-EOF
+
+data "aws_iam_policy_document" "iam_policy_for_S3_doc" {
+  statement {
+    actions = [ 
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject",
+    ]
+    effect = "Allow"
+    resources = [ "arn:aws:s3:::umassdininginfo/*" ]
+  }
 }
+
 
 resource "aws_iam_policy" "iam_policy_for_S3" {
   name = "aws_iam_policy_for_lambda_to_access_S3"
   path = "/"
+  policy = data.aws_iam_policy_document.iam_policy_for_S3_doc.json
+}
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:GetObject",
-        "s3:ListBucket",
-        "s3:PutObject"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "arn:aws:s3:::EXAMPLE-BUCKET/*"
-      ]
-    }
-  ]
+data "aws_iam_policy_document" "secrets_manager_policy_doc" {
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = [
+      "arn:aws:secretsmanager:us-east-2:717279727567:secret:prod/foodfinder*"
+    ]
+    effect = "Allow"
+  }
 }
-EOF
+
+resource "aws_iam_policy" "secrets_manager_policy" {
+  name = "aws_iam_policy_for_lambda_to_access_secrets_manager"
+  path = "/"
+  policy = data.aws_iam_policy_document.secrets_manager_policy_doc.json
 }
+
+resource "aws_iam_role_policy_attachment" "attach_iam_secrets_manager_policy_to_iam_role" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.secrets_manager_policy.arn
+}
+
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
  role        = aws_iam_role.lambda_role.name
@@ -73,23 +89,77 @@ resource "aws_iam_role_policy_attachment" "attach_iam_S3_policy_to_iam_role" {
   policy_arn = aws_iam_policy.iam_policy_for_S3.arn
 }
 
-resource "null_resource" "deployment_files" {
+resource "terraform_data" "build_lambda_scraper" {
   provisioner "local-exec" {
-    
+    command = "../scripts/create_pkg.sh"
+    working_dir = path.module
   }
 }
 
 data "archive_file" "zip_scraper" {
-type        = "zip"
-source_dir  = "${path.module}/../lambdas/scraper"
-output_path = "${path.module}/scraper.zip"
+  depends_on = [ terraform_data.build_lambda_scraper ]
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_dist_pkg"
+  output_path = "${path.module}/scraper.zip"
 }
 
 resource "aws_lambda_function" "scraper_lambda" {
-filename                       = "${path.module}/scraper.zip"
-function_name                  = "scraper_lambda_function"
-role                           = aws_iam_role.lambda_role.arn
-handler                        = "index.lambda_handler"
-runtime                        = "python3.8"
-depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename      = "${path.module}/scraper.zip"
+  function_name = "scraper_lambda_function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.13"
+  timeout       = 300
+
+}
+
+resource "aws_iam_role" "scraper_scheduler_role" {
+  name = "ScraperSchedulerRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "scraper_invoke_policy" {
+  name = "ScraperInvokeLambdaPolicy"
+  role = aws_iam_role.scraper_scheduler_role.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowEventBridgeToInvokeLambda",
+        "Action" : [
+          "lambda:InvokeFunction"
+        ],
+        "Effect" : "Allow",
+        "Resource" : aws_lambda_function.scraper_lambda.arn
+      }
+    ]
+  })
+}
+
+resource "aws_scheduler_schedule" "scraper_lambda_schedule" {
+  name = "ScraperLambdaSchedule"
+  flexible_time_window {
+    mode = "OFF"
+  }
+  schedule_expression = "cron(5 0 * * ? *)"
+  schedule_expression_timezone = "America/New_York"
+  target {
+    arn = aws_lambda_function.scraper_lambda.arn
+    role_arn = aws_iam_role.scraper_scheduler_role.arn
+    input = jsonencode({
+      "input": "This message was sent using EventBridge Scheduler!"
+    })
+  }
 }
